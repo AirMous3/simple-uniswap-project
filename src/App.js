6f8b2479c2aabf454e4e5861fb6fb95eabc19bcd
AirMous3 <airmouselol@gmail.com>
2022-04-07 10:35:26 +0300

    simple uniswap project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {useQuery} from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+export const client = new ApolloClient({
+    link: new HttpLink({
+        uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+    }),
+    fetchOptions: {
+        mode: 'no-cors',
+    },
+    cache: new InMemoryCache(),
+});
+
+const DAI_QUERY = gql`
+  query tokens($tokenAddress: Bytes!) {
+    tokens(where: { id: $tokenAddress }) {
+      derivedETH
+      totalLiquidity
+    }
+  }
+`;
+
+const ETH_PRICE_QUERY = gql`
+  query bundles {
+    bundles(where: { id: "1" }) {
+      ethPrice
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {loading: ethLoading, data: ethPriceData} = useQuery(ETH_PRICE_QUERY);
+    const {loading: daiLoading, data: daiData} = useQuery(DAI_QUERY, {
+        variables: {
+            tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
+        },
+    });
+
+    const daiPriceInEth = daiData && daiData.tokens[0].derivedETH;
+    const daiTotalLiquidity = daiData && daiData.tokens[0].totalLiquidity;
+    const ethPriceInUSD = ethPriceData && ethPriceData.bundles[0].ethPrice;
+
+    return (
+        <div>
+            {/*<iframe*/}
+            {/*    src="https://app.uniswap.org/#/swap?outputCurrency=0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359"*/}
+            {/*    height="660px"*/}
+            {/*    width="100%"*/}
+            {/*    style={{*/}
+            {/*        border: '0',*/}
+            {/*        marginBottom: '.5rem',*/}
+            {/*        display: 'block',*/}
+            {/*        borderRadius: '10px',*/}
+            {/*        maxWidth: '960px',*/}
+            {/*        minWidth: '300px',*/}
+            {/*    }}*/}
+            {/*/>*/}
+            <div>
+                Dai price:{' '}
+                {ethLoading || daiLoading
+                    ? 'Loading token data...'
+                    : '$' +
+                    // parse responses as floats and fix to 2 decimals
+                    (parseFloat(daiPriceInEth) * parseFloat(ethPriceInUSD)).toFixed(2)}
+            </div>
+            <div>
+                Dai total liquidity:{' '}
+                {daiLoading
+                    ? 'Loading token data...'
+                    : // display the total amount of DAI spread across all pools
+                    parseFloat(daiTotalLiquidity).toFixed(0)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
